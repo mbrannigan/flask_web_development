9027b8aae02e32ba68246fffa82b073ba22b5da9
Mike Brannigan <mikejbrannigan@gmail.com>
2025-08-10 20:54:57 -0500

    - Utilize Bootstrap web framework

--- ch03/hello.py
+++ ch03/hello.py
@@ -1,14 +1,16 @@
+from flask_bootstrap import Bootstrap
 from flask import Flask, render_template
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 #@app.route("/")
 #def index():
 #    return render_template("index.html")
 @app.route("/")
 def index():
-    return render_template("index.jinja2")
+    return render_template("index.html", name="World")
 
 @app.route("/user/<name>")
 def user(name):
-    return render_template("user.html", name=name)
+    return render_template("index.html", name=name)
